4110a31ba19f2fbd78927a0da3ba36d32a4cdf34
Rhafael Chandra <rhafaelchandra@gmail.com>
2024-12-31 08:26:28 +0700

    zen browser bug workaorund

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -99,7 +99,6 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
--- modules/home-manager/programs/zen-browser/default.nix
+++ modules/home-manager/programs/zen-browser/default.nix
@@ -1,14 +1,19 @@
 {
   config,
   pkgs,
-  system,
   inputs,
   ...
 }:
+
 let
   system = "x86_64-linux";
 in {
+  home.sessionVariables = {
+    MOZ_ENABLE_WAYLAND = "0";
+  };
+
   home.packages = [
     inputs.zen-browser.packages."${system}".default
   ];
 }
+
