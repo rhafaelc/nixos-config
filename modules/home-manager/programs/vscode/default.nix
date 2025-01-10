64d0869ca3528799a1187d85882fa21ed68d4888
Rhafael Chandra <rhafaelchandra@gmail.com>
2025-01-10 09:03:18 +0700

    feat(vscode): minimum extensions and adjust color theme

--- modules/home-manager/programs/vscode/default.nix
+++ modules/home-manager/programs/vscode/default.nix
@@ -1,11 +1,25 @@
 { 
   pkgs,
+  lib,
   ...
 }:{
   programs.vscode = {
     enable = true;
+
     extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
+      catppuccin.catppuccin-vsc-icons
+      catppuccin.catppuccin-vsc
+      github.copilot
+      github.copilot-chat
+      eamodio.gitlens
     ];
+
+    userSettings = {
+      "workbench.iconTheme"= "catppuccin-mocha";
+      "workbench.colorTheme"= lib.mkForce "Catppuccin Mocha";
+      "editor.fontFamily"= lib.mkForce "JetBrainsMono Nerd Font";
+      "terminal.integrated.fontFamily"= lib.mkForce "JetBrainsMono Nerd Font";
+    };
   };
 }
