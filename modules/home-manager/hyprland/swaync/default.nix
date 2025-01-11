06b3d0711e03c587f3ce82996a12d23720f77333
Rhafael Chandra <rhafaelchandra@gmail.com>
2025-01-11 17:57:10 +0700

    feat: update waybar and notification system configurations

--- modules/home-manager/hyprland/swaync/default.nix
+++ modules/home-manager/hyprland/swaync/default.nix
@@ -1,8 +1,32 @@
 {
   lib,
+  config,
   ...
 }: {
   services.swaync = {
     enable = true;
+    settings = {
+      "$schema" = "/etc/xdg/swaync/configSchema.json";
+      positionX = "right";
+      positionY = "top";
+      control-center-margin-top = 8;
+      control-center-margin-bottom = 8;
+      control-center-margin-right = 8;
+      notification-icon-size = 64;
+      notification-body-image-height = 100;
+      notification-body-image-width = 200;
+      timeout = 10;
+      timeout-low = 5;
+      timeout-critical = 0;
+      fit-to-screen = true;
+      control-center-width = 500;
+      notification-window-width = 500;
+      keyboard-shortcuts = true;
+      image-visibility = "when-available";
+      transition-time = 200;
+      hide-on-clear = true;
+      hide-on-action = true;
+      script-fail-notify = true;
+    };
   };
 }
