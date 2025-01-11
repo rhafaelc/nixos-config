ec929605edc1d9dddb18cdfff74178f65803692d
Rhafael Chandra <rhafaelchandra@gmail.com>
2025-01-11 17:57:03 +0700

    feat(rofi): reorganize themes and add new configurations

--- modules/home-manager/hyprland/rofi/default.nix
+++ modules/home-manager/hyprland/rofi/default.nix
@@ -1,19 +1,17 @@
+{ pkgs, config, ... }:
+
 {
-  lib,
-  config,
-  ...
-}: {
   programs.rofi = {
-    enable = true;
-    # font = lib.mkForce "JetBrainsMono Nerd Font";
-    theme = lib.mkForce ./catppuccin-mocha.rasi;
-    extraConfig = {
-      display-drun= "Applications:";
-      display-window= "Windows:";
-      drun-display-format= "{icon} {name}";
-      font= "JetBrainsMono Nerd Font Medium 10";
-      modi= "window,run,drun";
-      show-icons= true;
-    };
+    enable = false;
+  };
+
+  home.packages = [ pkgs.rofi-wayland ];
+
+  xdg.configFile = {
+    "rofi/config.rasi".source = ./config.rasi;
+    "rofi/themes/catppuccin-mocha.rasi".source = ./themes/catppuccin-mocha.rasi;
+    "rofi/themes/launcher.rasi".source = ./themes/launcher.rasi;
+    "rofi/themes/clipboard.rasi".source = ./themes/clipboard.rasi;
+    "rofi/themes/screenshot.rasi".source = ./themes/screenshot.rasi;
   };
 }
