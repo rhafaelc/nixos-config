cd5495481f5e499f97080bcbf3ae010cef1988cb
Rhafael Chandra <rhafaelchandra@gmail.com>
2025-01-12 20:18:40 +0700

    fix(tmux): home-manager installation for catppuccin theme

--- modules/home-manager/terminal/tmux.nix
+++ modules/home-manager/terminal/tmux.nix
@@ -1,9 +1,5 @@
 { pkgs, ... }:
 {
-  xdg.configFile = {
-    "tmux/plugins/catppuccin/tmux".source = ./plugins/catppuccin/tmux;
-  };
-
   programs.tmux = {
     enable = true;
     shell = "${pkgs.zsh}/bin/zsh";
@@ -13,7 +9,6 @@
     terminal = "tmux-256color";
     extraConfig = ''
       set-option -g status-position top
-      run ~/.config/tmux/plugins/catppuccin/tmux/catppuccin.tmux
     '';
 
 
@@ -27,18 +22,10 @@
         plugin = tmuxPlugins.catppuccin;
         extraConfig = ''
           set -g @catppuccin_flavor "mocha"
-
-          set -g @catppuccin_window_left_separator ""
-          set -g @catppuccin_window_right_separator " "
-
-          set -g @catppuccin_window_middle_separator " █"
-          set -g @catppuccin_window_number_position "right"
-
-          set -g @catppuccin_window_default_fill "number"
-          set -g @catppuccin_window_default_text "#W"
-
-          set -g @catppuccin_window_current_fill "number"
-          set -g @catppuccin_window_current_text "#W"
+          
+          set -g @catppuccin_window_status_style "basic"
+          set -ogq @catppuccin_window_default_text " #W"
+          set -ogq @catppuccin_window_current_text " #W"
 
           set -g @catppuccin_status_modules_right "directory host session"
           set -g @catppuccin_status_left_separator  " "
