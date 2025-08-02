6e3d70ad0242b024be09aaef0b66afc14337b3cd
Rhafael Chandra <rhafaelchandra@gmail.com>
2025-08-02 13:14:53 +0700

    update & disable flatpak apps

--- modules/home-manager/programs/flatpak/default.nix
+++ modules/home-manager/programs/flatpak/default.nix
@@ -3,13 +3,13 @@
     inputs.nix-flatpak.homeManagerModules.nix-flatpak
   ];
   services.flatpak.packages = [
-    {
-      appId = "org.vinegarhq.Sober";
-      origin = "flathub";
-    }
-    {
-      appId = "org.vinegarhq.Vinegar";
-      origin = "flathub";
-    }
+    # {
+    #   appId = "org.vinegarhq.Sober";
+    #   origin = "flathub";
+    # }
+    # {
+    #   appId = "org.vinegarhq.Vinegar";
+    #   origin = "flathub";
+    # }
   ];
 }
