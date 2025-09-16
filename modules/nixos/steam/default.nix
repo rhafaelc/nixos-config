97a5522961d23035fac1d5a60e7c73764f9a5eba
Rhafael Chandra <rhafaelchandra@gmail.com>
2025-09-16 07:40:09 +0700

    add wine

--- modules/nixos/steam/default.nix
+++ modules/nixos/steam/default.nix
@@ -5,4 +5,10 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+
+  environment.systemPackages = with pkgs; [
+    wineWowPackages.staging
+    winetricks
+    wineWowPackages.waylandFull
+  ];
 }
