ece133e71f8c9876d6807545c94619598e945bea
Rhafael Chandra <rhafaelchandra@gmail.com>
2026-07-18 16:55:42 +0900

    remove btop, hypridle, hyprlock, hyprshell, hyprsome, rofi, swaync, waybar

--- modules/home-manager/hyprland/default.nix
+++ modules/home-manager/hyprland/default.nix
@@ -2,18 +2,6 @@
   imports = [
     ./hypr
     ./noctalia
-
-    ./btop
-    ./rofi
-    ./waybar
-    ./swaync
-    ./hyprlock
-    ./hypridle
-    # ./hyprshell
-    ./hyprsome
-
-    ./wlogout
-    ./scripts/clipboard-history.nix
     ./satty
   ];
 }
