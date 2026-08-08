4bbdb806c0b0faee63628f3611cca7f6e899faf3
Rhafael Chandra <rhafaelchandra@gmail.com>
2026-08-08 11:49:55 +0900

    feat(nixos): enable steam as a leaner gaming module

--- nixos/gaming/default.nix
+++ nixos/gaming/default.nix
@@ -1,14 +1,8 @@
-{ pkgs, ... }: {
+{ ... }: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
-
-  environment.systemPackages = with pkgs; [
-    wineWowPackages.staging
-    winetricks
-    wineWowPackages.waylandFull
-  ];
 }
