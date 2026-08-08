eaa9b64063cc6b08c89659831a2bd9c0749a48bb
Rhafael Chandra <rhafaelchandra@gmail.com>
2026-08-08 10:44:37 +0900

    chore(hypr): remove orphaned mocha.conf, toggle_waybar.sh, and dead thunar module

--- modules/home-manager/hyprland/thunar/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  programs = {
-    thunar.enable = true;
-    xfconf.enable = true;
-  };
-  services.gvfs.enable = true; # Mount, trash, and other functionalities
-  services.tumbler.enable = true; # Thumbnail support for images
-}
