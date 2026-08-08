fc1b94213203a078c405993625735e55d5fc0b61
Rhafael Chandra <rhafaelchandra@gmail.com>
2026-08-08 18:59:46 +0900

    fix(themes): change to adawaita

--- home/system/themes/default.nix
+++ home/system/themes/default.nix
@@ -8,8 +8,8 @@
     enable = true;
     gtk.enable = true;
     x11.enable = true;
-    package = pkgs.yaru-theme;
-    name = "Yaru";
+    package = pkgs.adwaita-icon-theme;
+    name = "Adwaita";
     size = 24;
   };
 
